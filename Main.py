2a56de6c38cf360b51107c0684d42ede3c6b8b3a
willwillapi <75849495+willwillapi@users.noreply.github.com>
2020-12-14 00:44:18 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,18 +5,24 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/getdata', methods=['GET'])
 def respond():
   try:
+    print(request.args)
     TICKER = request.args.get('ticker')
     return yf.Ticker(str(TICKER)).info
-  except:
+  except Exception as e:
+    print(e)
     return {}
+
 
 @app.route('/', methods=['GET'])
 def rooturl():
+  print("ROOT URL")
   return '.'
-  
+
+
 if __name__ == '__main__':
   port = int(os.environ.get("PORT", 33333))
   app.run(threaded=True, host='0.0.0.0', port=port)
